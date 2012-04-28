f71773fe0e3a1b5f82a7820f5a6000024417fe86
Guilherme Vitor <stockholder911@gmail.com>
2012-04-28 15:54:36 -0300

    Moved functionality to return the active jobs. Instead it was transferred to the model in order to follow the MVC paradigm

--- lib/model/doctrine/JobeetJobTable.class.php
+++ lib/model/doctrine/JobeetJobTable.class.php
@@ -7,13 +7,20 @@
  */
 class JobeetJobTable extends Doctrine_Table
 {
-    /**
-     * Returns an instance of this class.
-     *
-     * @return object JobeetJobTable
-     */
-    public static function getInstance()
-    {
-        return Doctrine_Core::getTable('JobeetJob');
-    }
+/**
+ * Returns an instance of this class.
+ *
+ * @return object JobeetJobTable
+ */
+  public static function getInstance()
+  {
+      return Doctrine_Core::getTable('JobeetJob');
+  }
+  
+  public function getActiveJobs()
+  {
+    $q = $this->createQuery('j')
+      ->where('j.expires_at > ?', date('Y-m-d H:i:s', time()));
+    return $q->execute();
+  }
 }
